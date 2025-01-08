4781e00618c94403e8537add9be49da568e35e63
Ukkim1230 <gustndhksk@gmail.com>
2025-01-08 12:40:03 +0900

    sd

--- src/d0108/Lotto3.java
+++ src/d0108/Lotto3.java
@@ -1,9 +1,19 @@
 package d0108;
 
+import java.util.Random;
+
 public class Lotto3 {
 	
 	public static void main(String[] args) {
+		int[] nums = new int[3];
+		Random r = new Random();
+		for(int i = 0; i<3; i++) {
+			nums[i] = r.nextInt(100);
+		}
+		for(int i=0;i<3;i++) {
+			System.out.println(nums[i]);
+		}
+		
 		
 	}
-
 }
